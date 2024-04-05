80102f71880ed231c822337d2ef6ff8d3cbeb771
Anurag12133 <anurajput62616@gmail.com>
2024-04-05 19:46:44 +0530

    Frontened

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Signin } from "./Pages/Signin";
 import { Signup } from "./Pages/Signup";
 import { Blog } from "./Pages/Blog";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Blogs } from "./Pages/blogs";
+import { Publish } from "./Pages/Publish";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
